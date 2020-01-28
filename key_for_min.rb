8ab8728eb99b4d36ddc320bca43b5f325ed78183
Halle Montgomery <h.rikkele.montgomery@gmail.com>
2020-01-28 19:47:32 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 ikea = {:chair => 25, :table => 85, :mattress => 450}
 def key_for_min_value(ikea)
+  lowest_value = nil 
+  lowest_key = nil
   
+  ikea.each do |
 
 end
